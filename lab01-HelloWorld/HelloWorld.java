b4f1129f81a6d3f302e22d4d8ad4cbe31b50a589
russelllaros <rkl218@lehigh.edu>
2014-08-29 00:14:59 +0000

    HelloWord

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+/////////////////////////////////////
+//Russell laros
+//Demo 0
+//Hello World Java Program
+//
+// class
+public class HelloWorld{
+// main method
+    public static void main(String[] args) { 
+
+// printing statement with box around it
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("#                       #");
+    System.out.println("#  Hello World 2014!!!  #");
+    System.out.println("#                       #");
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("test");
+    
+    //test
+    }
+}
