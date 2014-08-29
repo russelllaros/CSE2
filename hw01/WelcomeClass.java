42fab0ec473ba0107d0e3cb3cb1d25f3776fe9b0
russelllaros <rkl218@lehigh.edu>
2014-08-29 03:06:52 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+/////////////////
+// RussellLaros
+// WelcomeClasshw01
+// class
+public class WelcomeClass{
+    // main method
+    public static void main(String[] args) {
+        
+    //printing Welcome Class statement
+    System.out.println("  ----------------");
+    System.out.println("   |   Welcome  | ");
+    System.out.println("  ----------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-R--K--L--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer I snorkeled with manta rays almost every night");
+    //test
+    }
+}
