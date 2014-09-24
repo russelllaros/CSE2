0c17b27029431692a6cf44e8024a76e80cb99a0d
russelllaros <rkl218@lehigh.edu>
2014-09-24 03:25:08 +0000

    updated Month

--- hw04/Month.java
+++ hw04/Month.java
@@ -2,10 +2,9 @@
 //Russell Laros
 //Sep23,2014
 //CSE02-hw04-IncomeTax
-//This program prompts the user to enter an int that 
-//is the thousands of dollars of income and then writes 
-//out the amount of tax on the income, given the following 
-//schedule:  <20, 5%;  >=20 and < 40, 7%; >=40 and < 78, 12%; >=78, 14%
+//Write a program that prompts the user to enter an integer //for the month (1 for January, 2 for February, etc.)  and then 
+//displays the number of days in the month)
+//If the number 2 is entered (February) the user is then asked to enter the year
 
 import java.util.Scanner;//Importing scanner
 
@@ -15,37 +14,49 @@ public class Month {//defining class
     myScanner = new Scanner( System.in ); //new scanner now can accept input
     
     
-    System.out.print("Enter an int for which month it is (1-12): ");
-    int month = myScanner.nextInt();
-        String monthString;
-        switch (month) {
-            case 1:  monthString = "There are 31 days in the month";
+    System.out.print("Enter an int for which month it is (1-12): ");//prompt for month
+    int month = myScanner.nextInt();//set integer for month from scanner
+       
+        if (month>12 || month<0); {//create error message conditions for months
+            System.out.println("Error! You did not put an int that corresponds to a month");
+        } else; {
+        int year = 0;//set variable for year
+        switch (month) {//switch statement
+            case 1: System.out.println("There are 31"); //set number of days
                 break;
-            case 2:  monthString = "There are 28 days in the month";
+            case 2: System.out.println("Enter the year"); 
+                year = myScanner.nextInt();
+                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {//create variable conditions for calculations for leap year
+                System.out.println("There are 29 days.");//show for number of days
+                return;
+                } else {
+                System.out.println("There are 28 days.");
+                }
                 break;
-            case 3:  monthString = "There are 31 days in the month";
+            case 3: System.out.println("There are 31 days "); 
                 break;
-            case 4:  monthString = "There are 30 days in the month";
+            case 4: System.out.println("There are 30 days "); 
                 break;
-            case 5:  monthString = "There are 31 days in the month";
+            case 5: System.out.println("There are 31 days"); 
                 break;
-            case 6:  monthString = "There are 30 days in the month";
+            case 6: System.out.println("There are 30 days");
                 break;
-            case 7:  monthString = "There are 31 days in the month";
+            case 7: System.out.println("There are 31 days"); 
                 break;
-            case 8:  monthString = "There are 31 days in the month";
+            case 8: System.out.println("There are 31 days"); 
                 break;
-            case 9:  monthString = "There are 30 days in the month";
+            case 9: System.out.println("There are 30 days");
                 break;
-            case 10:  monthString = "There are 31 days in the month";
+            case 10: System.out.println("There are 31 days"); 
                 break;
-            case 11:  monthString = "There are 30 days in the month";
+            case 11: System.out.println("There are 30 days"); 
                 break;
-            case 12:  monthString = "There are 31 days in the month";
+            case 12: System.out.println("There are 30 days"); 
                 break;
-        
+            default: 
+                break;
             }
-            System.out.println(monthString);
-        
-    }
-}
+    
+        }
+    }//end of main method
+}//end of class
