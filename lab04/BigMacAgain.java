3c95db621a86c753b8d16b9910c070f7a46e1f97
russelllaros <rkl218@lehigh.edu>
2014-09-26 00:11:44 +0000

    updated BigMacAgain

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -13,33 +13,33 @@ public class BigMacAgain { //adding class - public
     myScanner = new Scanner( System.in ); //new scanner now can accept input
     
     System.out.print("Enter number of Big Macs (an integer > 0): ");//prompt for number of big macs
-    int nBigMacs = myScanner.nextInt();
+    int nBigMacs = myScanner.nextInt();//set scanner to read int entered above
     
-    double priceBigMac$=2.22;
-    double priceFries$=2.15;
-    double priceOfBigMacs$=(nBigMacs * priceBigMac$);
-    double priceOfMealMacAndFries$=(priceFries$ + priceOfBigMacs$);
+    double priceBigMac$=2.22;//make variabe for price of BigMac
+    double priceFries$=2.15;//make variable for price of Fries
+    double priceOfBigMacs$=(nBigMacs * priceBigMac$);//make variable that is price of bigmcs times number of BMs ordered
+    double priceOfMealMacAndFries$=(priceFries$ + priceOfBigMacs$);//make variable that is total of order BMs plus order of fries
     
-    if (nBigMacs >= 0) {
+    if (nBigMacs >= 0) {//set if oder is 0 or greater print order total of BMs
         System.out.printf("You ordered "+nBigMacs+ " BigMacs for a total of %.2f\n", priceOfBigMacs$);
-    } else if (nBigMacs < 0) {
+    } else if (nBigMacs < 0) { //set if else number of BMs is less than one print improper int statement
         System.out.print("You did not enter an int > 0");
-    } else {
+    } else { //else cannot be an int print you did not enter an int
         System.out.print("you did not eneter in a proper integer");
     }
     
     System.out.print("Would you like an order of fries? (Y,N,y,n) ");//prompt if they want fries
-    String wantFries = myScanner.next();
-    switch (wantFries) {
-        case "Y": System.out.printf("You ordered fries at $2.15 for an order total of %.2f\n", priceOfMealMacAndFries$);
+    String wantFries = myScanner.next();//read string variable that was entered
+    switch (wantFries) { //set switch statement using wantFries variable
+        case "Y": System.out.printf("You ordered fries at $2.15 for an order total of %.2f\n", priceOfMealMacAndFries$);//case "Y" print price of BMs ordered plus fries
             break;
-        case "y": System.out.printf("You ordered fries at $2.15 for an order total of %.2f\n", priceOfMealMacAndFries$);
+        case "y": System.out.printf("You ordered fries at $2.15 for an order total of %.2f\n", priceOfMealMacAndFries$);//case "y" print price of BMs ordered plus fries
             break;
-        case "N": System.out.printf("You did not order fries so your order total is %.2f\n", priceOfBigMacs$);
+        case "N": System.out.printf("You did not order fries so your order total is %.2f\n", priceOfBigMacs$);//case "N" print just price of BMs ordered
             break;
-        case "n": System.out.printf("You did not order fries so your order total is %.2f\n", priceOfBigMacs$);
+        case "n": System.out.printf("You did not order fries so your order total is %.2f\n", priceOfBigMacs$);//case "n" print just price of BMs ordered
             break;
-        default: System.out.printf("!!Please enter in a valid character fro you choice!!\n");
+        default: System.out.printf("!!Please enter in a valid character for you choice!!\n");//something else entered print that you done effed up
             break;
     }
     
