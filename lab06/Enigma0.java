1bdd41753790b8b61bf6567d510fca0dcff21bbc
russelllaros <rkl218@lehigh.edu>
2014-10-01 16:37:04 +0000

    Updated Enigma0

--- lab06/Enigma0.java
+++ lab06/Enigma0.java
@@ -2,6 +2,7 @@
 //Russell Laros
 //CSE02-lab05-Enigma0
 //debugging
+//10-1-14
 
 
 /*Sample of expected output:
@@ -13,36 +14,31 @@
 
 import java.util.Scanner;
 public class Enigma0{
-  public static void main(String args []){
+  public static void main(String arg []){
     System.out.print("Enter an int- ");
     Scanner scan=new Scanner(System.in);
+    int n;
     if(scan.hasNextInt()){
-      int n=scan.nextInt();
+      n=scan.nextInt();
       System.out.println("You entered "+n);
-      int k=4,p=6,q=7,r=8;
-        switch(k+p+q+r){
-            case 24: 
-            case 25: System.out.println("sum is 25");
-            default:
-            System.out.println("To repeat, you entered "+n);
-        }        
-    }else{
-      int n=4;
+    }
+    else{
+      n=4;
       System.out.println("You entered "+n);
-      int k=4,p=6,q=7,r=8;
-        switch(k+p+q+r){
-            case 24: 
-            case 25: System.out.println("sum is 25");
-            default:
-            System.out.println("To repeat, you entered "+n);
-        }
+    }
     
+    int k=4,p=6,q=7,r=8;
+    switch(k+p+q+r){
+      case 24: 
+      case 25: System.out.println("sum is 25");
+      default:
+        System.out.println("To repeat, you entered "+n);
     }
   }
 }
+
 /* Error report:
  *   Expand this comment:
- *   n was not initializing in the last print line because n was being set up inside the if statement. 
- *   So I put the entire switch inside the if or else so n was initialized correctly and then would be print out 
- *   correctly for the value entered and then read by the scanner.
+ *   n was not initializing in the last print line because n was being stated inside the if statement. 
+ *   So I set the variable n outside the if statements so it can initialize
  */
